7518847013ad5324910fc6ddc72c5c9be88fbf88
david <david4rtz@gmail.com>
2025-10-24 10:52:29 +0700

    Telegram Optmize

--- src/main/java/com/bourong/telegrambot/MyTelegramBot.java
+++ src/main/java/com/bourong/telegrambot/MyTelegramBot.java
@@ -1,3 +1,36 @@
+//package com.bourong.telegrambot;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//import com.pengrad.telegrambot.TelegramBot;
+//import com.pengrad.telegrambot.request.SendMessage;
+//import com.pengrad.telegrambot.response.SendResponse;
+//
+//import lombok.Data;
+//
+//@Service
+//@Data
+//public class MyTelegramBot {
+//
+//	@Value("${token}")
+//	private String token;
+//	@Value("${chat_id}")
+//	private long chat_id;
+//	
+//	private TelegramBot bot;
+//	
+//	public SendResponse message(String text) {
+//		
+//		if(bot == null) {
+//			bot = new TelegramBot(token);
+//		}
+//		
+//		SendResponse sendMessage = bot.execute(new SendMessage(chat_id, text));
+//		return sendMessage;
+//	}
+//}
+
 package com.bourong.telegrambot;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -7,26 +40,23 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 
-import lombok.Data;
-
 @Service
-@Data
 public class MyTelegramBot {
 
-	@Value("${token}")
-	private String token;
-	@Value("${chat_id}")
-	private long chat_id;
-	
-	private TelegramBot bot;
-	
-	public SendResponse message(String text) {
-		
-		if(bot == null) {
-			bot = new TelegramBot(token);
-		}
-		
-		SendResponse sendMessage = bot.execute(new SendMessage(chat_id, text));
-		return sendMessage;
-	}
+    @Value("${TELEGRAM_BOT_TOKEN}")
+    private String token;
+
+    @Value("${TELEGRAM_CHAT_ID}")
+    private long chat_id;
+
+    private TelegramBot bot;
+
+    public SendResponse message(String text) {
+        if (bot == null) {
+            bot = new TelegramBot(token);
+        }
+        SendResponse sendMessage = bot.execute(new SendMessage(chat_id, text));
+        return sendMessage;
+    }
 }
+
